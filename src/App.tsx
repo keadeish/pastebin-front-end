bd559594e905c8e5a08abdc4e0f100f881673e84
syed <user.email>
2022-09-06 10:15:04 +0100

    added basic styling plus input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { greet } from "./utils/greet";
-
 function App(): JSX.Element {
-  return <h1>{greet("Everyone")}</h1>;
+  return (
+    <form className="new-paste-form">
+      <input className="title-text" placeholder="Title..."></input>
+      <input className="name-text" placeholder="Name..."></input>
+      <input className="text-area" placeholder="Text Area..."></input>
+      <button className="submit-button">SUBMIT</button>
+    </form>
+  );
 }
 
 export default App;
