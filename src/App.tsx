17349fc748e57af91a2ec62cb86683b29bf45103
syed <user.email>
2022-09-06 10:55:35 +0100

    format

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,46 @@ function App(): JSX.Element {
 
   useEffect(() => {
     axios.get("https://pastebin-back-end.herokuapp.com/pastes").then((res) => {
-      console.log(res.data)
+      console.log(res.data);
     });
   }, []);
 
   const postToBackend = () => {
-    axios.post("https://pastebin-back-end.herokuapp.com/pastes",{
-      name: nameText,
-      text: textArea,
-      title: titleText,
-      time: 'NOW()'
-    }).then((res) => {
-      console.log('SUBMITTED!!')
-    });
-  }
-  
-  
+    axios
+      .post("https://pastebin-back-end.herokuapp.com/pastes", {
+        name: nameText,
+        text: textArea,
+        title: titleText,
+        time: "NOW()",
+      })
+      .then((res) => {
+        console.log("SUBMITTED!!");
+      });
+  };
+
   return (
     <form className="new-paste-form">
-      <input className="title-text" placeholder="Title..." value={titleText} onChange={(e) => setTitleText(e.target.value)}></input>
-      <input className="name-text" placeholder="Name..." value={nameText} onChange={(e) => setNameText(e.target.value)}></input>
-      <input className="text-area" placeholder="Text Area..." value={textArea} onChange={(e) => setTextArea(e.target.value)}></input>
-      <button className="submit-button" onClick={()=>postToBackend()}>SUBMIT</button>
+      <input
+        className="title-text"
+        placeholder="Title..."
+        value={titleText}
+        onChange={(e) => setTitleText(e.target.value)}
+      ></input>
+      <input
+        className="name-text"
+        placeholder="Name..."
+        value={nameText}
+        onChange={(e) => setNameText(e.target.value)}
+      ></input>
+      <input
+        className="text-area"
+        placeholder="Text Area..."
+        value={textArea}
+        onChange={(e) => setTextArea(e.target.value)}
+      ></input>
+      <button className="submit-button" onClick={() => postToBackend()}>
+        SUBMIT
+      </button>
     </form>
   );
 }
