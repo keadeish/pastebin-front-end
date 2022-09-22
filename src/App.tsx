766b0682ee091294c99bf904645a1f8e2436d10b
syed <user.email>
2022-09-22 09:47:26 +0100

    cleaned up repo and some css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Paste } from "./Paste";
-import { pastesJSON } from "./utils/pasteList";
+import { Paste } from "./Components/Paste";
+import { pastesJSON } from "./utils/types/pasteList";
+import PasteForm from "./Components/PasteForm";
 
 function App(): JSX.Element {
-  const [titleText, setTitleText] = useState("");
-  const [nameText, setNameText] = useState("");
-  const [textArea, setTextArea] = useState("");
+  
   const [recentPastesArray, setRecentPastesArray] = useState<pastesJSON[]>([]);
-
-  // useEffect(() => {
-  //   axios.get("https://pastebin-back-end.herokuapp.com/pastes").then((res) => {
-  //     console.log(res.data);
-  //   });
-  // }, []);
+  const [refresh, setRefresh] = useState(false)
+  const [renderPasteForm, setRenderPasteForm] = useState(true)
 
   useEffect(() => {
     axios
@@ -21,55 +16,11 @@ function App(): JSX.Element {
       .then((res) => {
         setRecentPastesArray(res.data);
       });
-  }, []);
-
-  const postToBackend = () => {
-    if (textArea === "") {
-      console.log("alert");
-    } else {
-      axios
-        .post("https://pastebin-back-end.herokuapp.com/pastes", {
-          name: nameText,
-          text: textArea,
-          title: titleText,
-          time: "NOW()",
-        })
-        .then(() => {
-          axios
-            .get(
-              "https://pastebin-back-end.herokuapp.com/pastes/recent10pastes"
-            )
-            .then((res) => setRecentPastesArray(res.data));
-        });
-    }
-  };
+  }, [refresh]);
 
   return (
-    <>
-      <div className="whole-page">
-        <div className="new-paste-form">
-          <input
-            className="title-text"
-            placeholder="Title..."
-            value={titleText}
-            onChange={(e) => setTitleText(e.target.value)}
-          ></input>
-          <input
-            className="name-text"
-            placeholder="Name..."
-            value={nameText}
-            onChange={(e) => setNameText(e.target.value)}
-          ></input>
-          <textarea
-            className="text-area"
-            placeholder="Text Area..."
-            value={textArea}
-            onChange={(e) => setTextArea(e.target.value)}
-          ></textarea>
-          <button className="submit-button" onClick={() => postToBackend()}>
-            SUBMIT
-          </button>
-        </div>
+    <div className="container">          
+        {renderPasteForm && <PasteForm setRefresh={setRefresh}/>}
         <div className="ten-pastes">
           {recentPastesArray.map((onePasteJSON: pastesJSON) => {
             return (
@@ -83,9 +34,8 @@ function App(): JSX.Element {
               />
             );
           })}
-        </div>
-      </div>
-    </>
+        </div>      
+    </div>
   );
 }
 
---
+++ src/utils/types/pasteForm.ts
@@ -0,0 +1,3 @@
+export interface pasteFormProps {
+    setRefresh: React.Dispatch<React.SetStateAction<boolean>>
+}
